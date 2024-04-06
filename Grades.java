9239fa135959c5b494f3d767f2d9f5397b0ea51c
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-04-06 15:18:39 +0200

    final ex2

--- Grades.java
+++ Grades.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Grades {
-    
+
     public static double calculateAverage(double[] grades) {
         double sumOfGrades = 0;
         for (double grade : grades) {
@@ -9,16 +9,11 @@ public class Grades {
         }
         return sumOfGrades / grades.length;
     }
-    
-    public static void enterGrades(Scanner scanner, double[] studentGrades, String subject) {
-        System.out.println("Enter the grades for " + subject + " (separated by space): ");
-        String input = scanner.nextLine().trim(); // Usuwa białe znaki z końca
-        if (input.endsWith(" ")) {
-            input = input.substring(0, input.length() - 1); // Usuwa ostatnią spację
-        }
-        String[] gradesInput = input.split("\\s+");
-        for (int i = 0; i < gradesInput.length; i++) {
-            studentGrades[i] = Double.parseDouble(gradesInput[i]);
+
+    public static void enterGradesForStudent(double[] studentGrades, Scanner scanner) {
+        for (int i = 0; i < studentGrades.length; i++) {
+            System.out.print("Enter grade for subject " + (i + 1) + ": ");
+            studentGrades[i] = scanner.nextDouble();
         }
     }
 
@@ -27,7 +22,7 @@ public class Grades {
 
         System.out.print("Enter the number of students: ");
         int numberOfStudents = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
 
         final int numberOfSubjects = 3;
         double[][] studentGrades = new double[numberOfStudents][numberOfSubjects];
@@ -37,15 +32,22 @@ public class Grades {
         for (int i = 0; i < numberOfStudents; i++) {
             System.out.println("\nStudent " + (i + 1));
             for (int j = 0; j < numberOfSubjects; j++) {
-                enterGrades(scanner, studentGrades[i], subjects[j]);
+                System.out.print("Enter the number of grades for " + subjects[j] + ": ");
+                int numGrades = scanner.nextInt();
+                double[] grades = new double[numGrades];
+                System.out.println("Enter grades for " + subjects[j] + ":");
+                enterGradesForStudent(grades, scanner);
+                studentGrades[i][j] = calculateAverage(grades);
             }
         }
 
+        System.out.println("\nAverage grades for each subject for each student:");
         for (int i = 0; i < numberOfStudents; i++) {
-            System.out.println("\nAverage grades for student " + (i + 1) + ":");
+            System.out.println("\nStudent " + (i + 1));
             for (int j = 0; j < numberOfSubjects; j++) {
-                System.out.println(subjects[j] + ": " + calculateAverage(studentGrades[i]));
+                System.out.println(subjects[j] + ": " + studentGrades[i][j]);
             }
+            System.out.println("Average grade: " + calculateAverage(studentGrades[i]));
         }
 
         scanner.close();
